09e6c4696309aa3685c0c0975ca9f4094145a899
midnighteagle <chiranjeevivaston@gmail.com>
2025-10-08 19:51:25 +0530

    Reapeated Interview Questions

---
+++ RepetedInterview.cpp
@@ -0,0 +1,17 @@
+# include<iostream>
+using namespace std;
+int main(){
+    // for (int i = 1; i <=5; i=i+1){
+    //     cout<<i<<endl; // it  goes on till infinity
+    // }
+
+    // if (5<=10){
+    //     cout<<"Hello World"<<endl;  // its print only once
+    // } 
+
+    int i;
+    if(cin>>i){
+        cout<<"Akshat"<<endl; // it doesnot print anything.
+    }
+    return 0;
+}
